ab427ac8b315582118f5ea4c4dd145f102d97ce2
Nicolas Videla <videlanicolas@gmail.com>
2017-06-26 23:46:51 -0300

    Finished SlidePuzzle

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Python Gamming tutorials
 Following tutorials from <a href=https://inventwithpython.com/makinggames.pdf>this guide</a> and <a href=http://thepythongamebook.com/en:pygame:start>this guide</a>.
 
-- [x] Basics (blank screen, draw primitives, animate primitives, display fonts, sounds, music)
-- [ ] Memory Puzzle
-- [ ] Slide Puzzle
+- [x] <a href="https://github.com/videlanicolas/pythongame/tree/master/basics">Basics</a> (blank screen, draw primitives, animate primitives, display fonts, sounds, music)
+- [x] <a href="https://github.com/videlanicolas/pythongame/tree/master/MemoryPuzzle">Memory Puzzle</a>
+- [x] <a href="https://github.com/videlanicolas/pythongame/tree/master/SlidePuzzle">Slide Puzzle</a>
 - [ ] Simulate
 - [ ] Wormy
 - [ ] Tetromino
@@ -13,3 +13,19 @@ Following tutorials from <a href=https://inventwithpython.com/makinggames.pdf>th
 - [ ] Ink spill
 - [ ] Connect four
 - [ ] Gemgem
+
+### How to play?
+
+Clone this repo:
+
+```
+sudo apt-get install -y git
+git clone https://github.com/videlanicolas/pythongame.git
+```
+
+Or download the game you want to play:
+
+```
+sudo apt-get install -y subversion
+git clone https://github.com/videlanicolas/pythongame/trunk/<Game's Folder>
+```
--- SlidePuzzle/slidepuzzle.py
+++ SlidePuzzle/slidepuzzle.py
@@ -2,6 +2,13 @@
 import pygame, sys, time, random, glob, os
 from pygame.locals import *
 from enum import Enum
+from itertools import chain
+
+def deepcopy(M):
+	aux = list()
+	for element in M:
+		aux.append(element)
+	return aux
 
 class Colors(Enum):
 	BLACK = (0,0,0)
@@ -71,25 +78,29 @@ class GameRect(object):
 			return False
 
 class Button(GameRect):
-	def __init__(self,surface,display,posx,posy,width,height,forecolor=Colors.GRAY,bgcolor=Colors.GRAY,pressedcolor=Colors.GRAY,legend='Button',fontcolor=Colors.BLACK,font='freesansbold.ttf',fontsize=32,sound=None):
+	def __init__(self,surface,display,posx,posy,width,height,buttonaction=None,forecolor=Colors.GRAY,bgcolor=Colors.GRAY,pressedcolor=Colors.GRAY,legend='Button',fontcolor=Colors.BLACK,font='freesansbold.ttf',fontsize=32,sound=None):
 		GameRect.__init__(self,surface,display,posx,posy,width,height,forecolor,bgcolor,legend,fontcolor,font,fontsize)
 		self.__pressedcolor = pressedcolor
 		self.__pressed = False
 		self.__sound = sound
+		self.__buttonaction = buttonaction
 		self.fontObj = pygame.font.Font(font, fontsize)
-	def animatepress(self,wait=0.1):
+	def animatepress(self,wait=0.1,classaction=None):
 		if self.__pressed:
 			self.unpress()
 		if self.__sound:
 			self.__sound.play()
-		self.press()
+		self.press(classaction)
 		time.sleep(wait)
 		self.unpress()
-	def press(self):
+	def press(self,classaction=None):
 		pygame.draw.rect(self._surface,self.__pressedcolor, (self._x,self._y,self._width,self._height))
 		self._drawtext()
 		self._display.update()
 		self.__pressed = True
+		if self.__buttonaction and classaction:
+			method = getattr(classaction, self.__buttonaction)
+			method()
 	def unpress(self):
 		pygame.draw.rect(self._surface,self._forecolor, (self._x,self._y,self._width,self._height))
 		self._drawtext()
@@ -97,68 +108,148 @@ class Button(GameRect):
 		self.__pressed = False
 
 class Slab(GameRect):
-	def __init__(self,surface,display,posx,posy,length,legend,bgcolor=Colors.GRAY,fontsize=32,color=Colors.YELLOW,sound=None):
+	def __init__(self,surface,display,posx,posy,length,legend,margin=5,bgcolor=Colors.GRAY,fontsize=32,color=Colors.YELLOW,sound=None):
 		GameRect.__init__(self,surface,display,posx,posy,length,length,color,bgcolor,legend,fontsize=fontsize)
 		self.__sound = sound
+		self.__length = length
+		self.__margin = margin
 		self.fontObj = pygame.font.Font(self._font, fontsize)
 	def __len__(self):
-		return self._length
+		return self.__length
 	def move(self,direction):
 		if direction == Direction.UP:
 			self.erase()
-			self._y -= self._height
+			self._y -= self._height + self.__margin
 			assert self._y >= 0
 			self.draw()
 		elif direction == Direction.DOWN:
 			self.erase()
-			self._y += self._height
+			self._y += self._height + self.__margin
 			self.draw()
 		elif direction == Direction.LEFT:
 			self.erase()
-			self._x -= self._width
+			self._x -= self._width + self.__margin
 			assert self._x >= 0
 			self.draw()
 		elif direction == Direction.RIGHT:
 			self.erase()
-			self._x += self._width
+			self._x += self._width + self.__margin
 			self.draw()
-		self.__sound.play()
-
+		if self.__sound:
+			self.__sound.play()
 
 class Container(GameRect):
 	def __init__(self,surface,display,posx,posy,width,height,color=Colors.BLACK,bgcolor=Colors.GRAY,rectwidth=5):
 		GameRect.__init__(self,surface,display,posx,posy,width,height,color,bgcolor,rectwidth=rectwidth)
 		self._objects = list()
 	def redrawall(self):
-		for obj in self.__objects:
+		for obj in self._objects:
 			assert isinstance(obj,GameRect)
 			obj.redraw()
 	def _addobject(self,obj):
 		assert obj in self
 		self._objects.append(obj)
-	def handleclick(self,x,y):
+	def handleclick(self,x,y,classaction=None):
 		for obj in self._objects:
 			if obj.mouseinside(x,y):
-				obj.animatepress()
-	def addButton(self,relx,rely,width,height,forecolor=Colors.GRAY,pressedcolor=Colors.GRAY,legend='Button',fontcolor=Colors.BLACK,font='freesansbold.ttf',fontsize=32,sound=None):
-		self._addobject(Button(self._surface,self._display,self._x+relx,self._y+rely,width,height,bgcolor=self._bgcolor,forecolor=forecolor,pressedcolor=pressedcolor,legend=legend,fontsize=fontsize,sound=sound))
-	def addSlab(self,relx,rely,length,legend,fontsize=32,color=Colors.YELLOW,sound=None):
-		self._addobject(Slab(self._surface,self._display,self._x + relx,self._y + rely,length,legend,bgcolor=self._bgcolor,fontsize=fontsize,color=color,sound=sound))
+				obj.animatepress(classaction=classaction)
+	def addButton(self,relx,rely,width,height,buttonaction=None,forecolor=Colors.GRAY,pressedcolor=Colors.GRAY,legend='Button',fontcolor=Colors.BLACK,font='freesansbold.ttf',fontsize=32,sound=None):
+		self._addobject(Button(self._surface,self._display,self._x+relx,self._y+rely,width,height,buttonaction=buttonaction,bgcolor=self._bgcolor,forecolor=forecolor,pressedcolor=pressedcolor,legend=legend,fontsize=fontsize,sound=sound))
 
 class Board(Container):
-	def __init__(self,surface,display,posx,posy,width,height,color=Colors.BLACK,bgcolor=Colors.GRAY,rectwidth=5,slabsize=32):
-		Container.__init__(surface,display,posx,posy,width,height,color=color,bgcolor=bgcolor,rectwidth=rectwidth)
-		self.__matrix = [None*4]*4
-		for number in random.shuffle([x for x in range(1,16)]):
-			i = 0
-			j = 0
-			for i in range(4): 
-				for j in range(4):
-					if not self.__matrix[i][j]:
-						self.__matrix[i][j] = number
-						
-
-
+	def __init__(self,surface,display,posx,posy,width,height,color=Colors.BLACK,bgcolor=Colors.GRAY,rectwidth=5,slabsize=32,margin=5):
+		Container.__init__(self,surface,display,posx,posy,width,height,color=color,bgcolor=bgcolor,rectwidth=rectwidth)
+		self.__matrix = None
+		self.__slabsize = slabsize
+		self.__margin = margin
+		self.initmatrix()
+		self.__backupmatrix = None
+		self.__makebackupmatrix()
+	def __makebackupmatrix(self):
+		self.__backupmatrix = list()
+		for i in self.__matrix:
+			self.__backupmatrix.append(i[:])
+	def initmatrix(self):
+		if self.__matrix:
+			self.eraseall()
+		self.__matrix = [[None for x in range(4)] for y in range(4)]
+		numbers = list()
+		for i in range(4):
+			for j in range(4):
+				if i == 3 and j == 3:
+					break
+				num = random.randint(1,15)
+				while num in numbers:
+					num = random.randint(1,15)
+				numbers.append(num)
+				self.__addSlab(i,j,str(num))
+		self.redrawall()
+	def resetmatrix(self):
+		self.eraseall()
+		self.__matrix = [[None for x in range(4)] for y in range(4)]
+		for i in range(4):
+			for j in range(4):
+				if i == 3 and j == 3:
+					continue
+				self.__addSlab(i,j,self.__backupmatrix[i][j].legend)
+		self.redrawall()
+	def redrawall(self):
+		for slab in chain.from_iterable(zip(*self.__matrix)):
+			if slab:
+				slab.redraw()
+		self.draw()
+	def eraseall(self):
+		for slab in chain.from_iterable(zip(*self.__matrix)):
+			if slab:
+				slab.erase()
+	def haswon(self):
+		winningmatrix = [['1','2','3','4'],['5','6','7','8'],['9','10','11','12'],['13','14','15',None]]
+		for i in range(4):
+			for j in range(4):
+				if not self.__matrix[i][j]:
+					if i != 3 or j != 3:
+						return False
+				elif winningmatrix[i][j] != self.__matrix[i][j].legend:
+					return False
+		return True
+	def handleclick(self,x,y):
+		for i in range(4):
+			for j in range(4):
+				if not self.__matrix[i][j]:
+					continue
+				if self.__matrix[i][j].mouseinside(x,y):
+					#Search the None
+					#Is it below?
+					if i < 3:
+						if not self.__matrix[i+1][j]:
+							self.__matrix[i][j].move(Direction.DOWN)
+							self.__matrix[i+1][j] = self.__matrix[i][j]
+							self.__matrix[i][j] = None
+							return
+					#Is it left?
+					if j > 0:
+						if not self.__matrix[i][j-1]:
+							self.__matrix[i][j].move(Direction.LEFT)
+							self.__matrix[i][j-1] = self.__matrix[i][j]
+							self.__matrix[i][j] = None
+							return
+					#Is it right?
+					if j < 3:
+						if not self.__matrix[i][j+1]:
+							self.__matrix[i][j].move(Direction.RIGHT)
+							self.__matrix[i][j+1] = self.__matrix[i][j]
+							self.__matrix[i][j] = None
+							return
+					#Is it up?
+					if i > 0:
+						if not self.__matrix[i-1][j]:
+							self.__matrix[i][j].move(Direction.UP)
+							self.__matrix[i-1][j] = self.__matrix[i][j]
+							self.__matrix[i][j] = None
+							return
+				
+	def __addSlab(self,i,j,legend,fontsize=32,color=Colors.YELLOW,sound=None):
+		self.__matrix[i][j] = Slab(self._surface,self._display,self._x+self.__margin*(j+1)+self.__slabsize*j,self._y+self.__margin*(i+1)+self.__slabsize*i,self.__slabsize,legend,margin=self.__margin,bgcolor=self._bgcolor,fontsize=fontsize,color=color,sound=sound)
 
 def main():
 	pygame.init()
@@ -171,31 +262,14 @@ def main():
 	SLABSIZE=100
 	MARGIN=5
 
+	board = Board(DISPLAYSURF,pygame.display,20,20,4*SLABSIZE+5*MARGIN,4*SLABSIZE+5*MARGIN,slabsize=SLABSIZE,margin=MARGIN,color=Colors.BLACK,bgcolor=BGCOLOR)
+	board.redrawall()
 
 	menu = Container(DISPLAYSURF,pygame.display,WINDOWWIDTH-170,20+4*SLABSIZE+5*MARGIN-150,150,150,color=Colors.BLACK,bgcolor=BGCOLOR)
 	menu.draw()
-	menu.addButton(25,20,100,50,forecolor=Colors.GRAY,pressedcolor=Colors.DARKGREY,legend='New Game',fontsize=16,sound=clicksound)
-	menu.addButton(25,85,100,50,forecolor=Colors.GRAY,pressedcolor=Colors.DARKGREY,legend='Reset',fontsize=16,sound=clicksound)
+	menu.addButton(25,20,100,50,buttonaction='initmatrix',forecolor=Colors.GRAY,pressedcolor=Colors.DARKGREY,legend='New Game',fontsize=16,sound=clicksound)
+	menu.addButton(25,85,100,50,buttonaction='resetmatrix',forecolor=Colors.GRAY,pressedcolor=Colors.DARKGREY,legend='Reset',fontsize=16,sound=clicksound)
 	menu.redrawall()
-
-	grid = Container(DISPLAYSURF,pygame.display,20,20,4*SLABSIZE+5*MARGIN,4*SLABSIZE+5*MARGIN,color=Colors.BLACK,bgcolor=BGCOLOR)
-	grid.draw()
-	grid.addSlab(MARGIN,MARGIN,SLABSIZE,'1')
-	grid.addSlab(MARGIN*2+SLABSIZE,MARGIN,SLABSIZE,'2')
-	grid.addSlab(MARGIN*3+SLABSIZE*2,MARGIN,SLABSIZE,'3')
-	grid.addSlab(MARGIN*4+SLABSIZE*3,MARGIN,SLABSIZE,'4')
-	grid.addSlab(MARGIN,MARGIN*2+SLABSIZE,SLABSIZE,'5')
-	grid.addSlab(MARGIN*2+SLABSIZE,MARGIN*2+SLABSIZE,SLABSIZE,'6')
-	grid.addSlab(MARGIN*3+SLABSIZE*2,MARGIN*2+SLABSIZE,SLABSIZE,'7')
-	grid.addSlab(MARGIN*4+SLABSIZE*3,MARGIN*2+SLABSIZE,SLABSIZE,'8')
-	grid.addSlab(MARGIN,MARGIN*3+SLABSIZE*2,SLABSIZE,'9')
-	grid.addSlab(MARGIN*2+SLABSIZE,MARGIN*3+SLABSIZE*2,SLABSIZE,'10')
-	grid.addSlab(MARGIN*3+SLABSIZE*2,MARGIN*3+SLABSIZE*2,SLABSIZE,'11')
-	grid.addSlab(MARGIN*4+SLABSIZE*3,MARGIN*3+SLABSIZE*2,SLABSIZE,'12')
-	grid.addSlab(MARGIN,MARGIN*4+SLABSIZE*3,SLABSIZE,'13')
-	grid.addSlab(MARGIN*2+SLABSIZE,MARGIN*4+SLABSIZE*3,SLABSIZE,'14')
-	grid.addSlab(MARGIN*3+SLABSIZE*2,MARGIN*4+SLABSIZE*3,SLABSIZE,'15')
-	grid.redrawall()
 
 	while True:
 		for event in pygame.event.get():
@@ -203,11 +277,20 @@ def main():
 				pygame.mixer.music.stop()
 				pygame.quit()
 				sys.exit()
-			elif event.type == KEYDOWN:
-				pass
 			elif event.type == MOUSEBUTTONDOWN:
 				x, y = event.pos
-				menu.handleclick(x,y)
+				menu.handleclick(x,y,board)
+				if board.mouseinside(x,y):
+					board.handleclick(x,y)
+		if board.haswon():
+			winningsign = GameRect(DISPLAYSURF,pygame.display,(WINDOWWIDTH-350)/2,(WINDOWHEIGHT-150)/2,350,150,forecolor=Colors.GREEN,legend='You Won!',fontsize=64)
+			winningsign.redraw()
+			while True:
+				for event in pygame.event.get():
+					if event.type in [QUIT,KEYDOWN,MOUSEBUTTONDOWN]:
+						pygame.mixer.music.stop()
+						pygame.quit()
+						sys.exit()
 		FPSCLOCK.tick(FPS)
 
 if __name__ == '__main__':
